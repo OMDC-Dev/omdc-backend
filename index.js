2d3a66f314ae18f88fa0f0021fa9d51be1ed29ed
ibnuputraard <ibnu.putra@nawadata.id>
2024-04-17 16:07:10 +0700

    update to 8.9

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ app.use(router);
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Reimbursement Apps Service v.0.8.7" });
+  res.json({ message: "Reimbursement Apps Service v.0.8.9" });
 });
 
 // set port, listen for requests
--- routes/index.js
+++ routes/index.js
@@ -30,28 +30,28 @@ router.delete("/dept/:id", dept.delete);
 
 // User
 router.post("/user/login", ruser.login);
-router.post("/user/complete", authenticateToken, ruser.completeUser);
-router.post("/user/update-password", authenticateToken, ruser.updatePw);
-router.post("/user/logout", authenticateToken, ruser.logout);
+router.post("/user/complete", ruser.completeUser);
+router.post("/user/update-password", ruser.updatePw);
+router.post("/user/logout", ruser.logout);
 
 // Reimbursement
 router.get("/cabang", reimbursement.cabang);
-router.post("/reimbursement", authenticateToken, reimbursement.reimbursement);
+router.post("/reimbursement", reimbursement.reimbursement);
 router.get(
   "/reimbursement",
-  authenticateToken,
+
   reimbursement.get_reimbursement
 );
 router.post(
   "/reimbursement/status/:id",
-  authenticateToken,
+
   reimbursement.acceptance
 );
 router.get("/reimbursement/status/:id", reimbursement.get_status);
-router.get("/reimbursement/:id", authenticateToken, reimbursement.get_detail);
+router.get("/reimbursement/:id", reimbursement.get_detail);
 router.delete(
   "/reimbursement/:id",
-  authenticateToken,
+
   reimbursement.cancel_upload
 );
 
@@ -66,8 +66,8 @@ router.get("/muser", muser.getUser);
 router.post("/superuser", superuser.createUser);
 router.get("/superuser", superuser.getUser);
 router.get("/superuser/userlist", superuser.getAllUsers);
-router.get("/superuser/pengajuan", authenticateToken, superuser.get_pengajuan);
-router.get("/superuser/user", authenticateToken, superuser.getUserDetail);
+router.get("/superuser/pengajuan", superuser.get_pengajuan);
+router.get("/superuser/user", superuser.getUserDetail);
 router.get("/superuser/reimbursement", reimbursement.get_super_reimbursement);
 router.get(
   "/superuser/reimbursement/report",
@@ -78,18 +78,18 @@ router.get("/superuser/barang", barang.getAllRequestBarangAdmin);
 // finance
 router.get(
   "/finance/pengajuan",
-  authenticateToken,
+
   superuser.get_pengajuan_finance
 );
 router.post(
   "/finance/acceptance/:id",
-  authenticateToken,
+
   reimbursement.finance_acceptance
 );
 router.delete("/superuser/delete/:iduser", superuser.deleteAdmin);
 router.post(
   "/finance/update-coa/:id",
-  authenticateToken,
+
   reimbursement.finance_update_coa
 );
 
@@ -97,26 +97,26 @@ router.post(
 router.get("/anakcabang", barang.getAllAnakCabang);
 router.get("/anakcabang/detail", barang.getCabangDetail);
 router.get("/barang", barang.getBarang);
-router.post("/barang/create", authenticateToken, barang.createTrxPermintaan);
-router.get("/barang/requested", authenticateToken, barang.getAllRequestBarang);
+router.post("/barang/create", barang.createTrxPermintaan);
+router.get("/barang/requested", barang.getAllRequestBarang);
 router.get("/barang/requested/detail", barang.getDetailPermintaan);
 
 // Pengumuman
-router.post("/pengumuman", authenticateToken, pengumuman.createPengumuman);
-router.get("/pengumuman", authenticateToken, pengumuman.getPengumuman);
+router.post("/pengumuman", pengumuman.createPengumuman);
+router.get("/pengumuman", pengumuman.getPengumuman);
 router.delete(
   "/pengumuman/:pid",
-  authenticateToken,
+
   pengumuman.deletePengumuman
 );
 router.get(
   "/pengumuman/count",
-  authenticateToken,
+
   pengumuman.getPengumumanCount
 );
 router.post(
   "/pengumuman/read/:id",
-  authenticateToken,
+
   pengumuman.readPengumuman
 );
 
