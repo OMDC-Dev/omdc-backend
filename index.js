a7214fce761583b7b19690fc54659d8d394673d4
ibnuputraard <ibnu.putra@nawadata.id>
2025-02-09 20:15:08 +0700

    update code version to 960

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const { Responder } = require("./utils/responder");
 
 const app = express();
 
-const CODE_VERSION = "9.6";
+const CODE_VERSION = "9.6.0";
 
 // var corsOptions = {
 //   origin: "http://localhost:5173",
